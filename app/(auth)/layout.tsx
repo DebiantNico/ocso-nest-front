cc3476ba59ccf33bbeb2f8a6438bf791b0244318
Emiliano Nicolas Mendoza Garay <mendozanikolas000@gmail.com>
2026-03-28 00:32:30 -0600

    Creación de login y sign up

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="bg-orange-200 w-screen h-screen overflow-hidden grid">
+      <div className="place-content-center place-self-center place-items-center text-center">
+        <Image 
+          className="absolute left-[50%] top-10"
+          src="public\oxxo-logo.svg" 
+          alt="Logo de Oxxo" 
+          width={300} 
+          height={0} 
+        />
+        {children}
+      </div>
+    </div>
+  );
+}
